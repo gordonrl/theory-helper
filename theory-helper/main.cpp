977f3cbf4a23f8389759e1af41b785acef326164
gordonrl <gordonrl@umich.edu>
2022-07-12 19:06:07 -0400

    initial commit hopefully

---
+++ theory-helper/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  theory-helper
+//
+//  Created by Gordon Lewis on 7/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
