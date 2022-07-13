31cdbbaff3c88d96b8c3be4883c545774f8ab21a
gordonrl <gordonrl@umich.edu>
2022-07-12 20:17:28 -0400

    Start, weak

---
+++ main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  theory-helper
+//
+//  Created by Gordon Lewis on 7/12/22.
+//
+
+#include <iostream>
+#include <utility>
+#include <string>
+
+using namespace std;
+
+/*
+ Part one of this project is creating an array of pairs for every key signature for
+ every interval. This will make it fairly easy to identify chord changes because they are
+ just intervals depending on the type of each chord.
+ 
+ The project will work by identifying the type of each chord and the interval between them.
+ Ex: Am-D7-Gmaj is a ii-V-I and you can tell becuse Am-D7 is the A is minor, the D is dominant,
+ and it's a perfect fourth between them.
+ */
+
+
+
+int main() {
+    cout << "Hello World!" << endl;
+    
+    return 0;
+}
